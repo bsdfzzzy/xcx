c1265134878fa8704504e54aee0840b274ecad94
Zhengyang Zhao <zyzhao@thoughtworks.com>
2018-03-06 00:13:41 +0800

    [Zhengyang] Add a button to get newest data.

--- src/store/actions/home.js
+++ src/store/actions/home.js
@@ -1,6 +1,22 @@
-import { GET_HOME_PAGE_INFORMATION } from '../types'
+import {
+  GET_HOME_PAGE_INFORMATION,
+  GET_NEW_DATA,
+  REQUEST_BEGIN,
+  REQUEST_FINISH
+} from '../types'
 import { createAction } from 'redux-actions'
 import wepy from 'wepy'
+import { store } from '../../app.wpy'
+
+export const requestBegin = {
+  type: REQUEST_BEGIN,
+  payload: ''
+}
+
+export const requestFinish = {
+  type: REQUEST_FINISH,
+  payload: ''
+}
 
 export const getHomePageInformation = createAction(GET_HOME_PAGE_INFORMATION, () => {
   return new Promise(resolve => {
@@ -8,6 +24,19 @@ export const getHomePageInformation = createAction(GET_HOME_PAGE_INFORMATION, ()
       url: 'http://localhost:8081/',
       header: { 'Content-Type': 'application/json' }
     }).then(result => {
+      resolve(result.data)
+    })
+  })
+})
+
+export const getNewData = createAction(GET_NEW_DATA, () => {
+  return new Promise(resolve => {
+    store.dispatch(requestBegin)
+    wepy.request({
+      url: 'http://localhost:8081/update',
+      header: { 'Content-Type': 'application/json' }
+    }).then(result => {
+      store.dispatch(requestFinish)
       resolve(result.data)
     })
   })
--- src/store/reducers/home.js
+++ src/store/reducers/home.js
@@ -1,5 +1,10 @@
 import { handleActions } from 'redux-actions'
-import { GET_HOME_PAGE_INFORMATION } from '../types'
+import {
+  GET_HOME_PAGE_INFORMATION,
+  GET_NEW_DATA,
+  REQUEST_BEGIN,
+  REQUEST_FINISH
+} from '../types'
 
 export default handleActions({
   [GET_HOME_PAGE_INFORMATION] (state, action) {
@@ -14,7 +19,34 @@ export default handleActions({
       ...state,
       plantLogs: result
     }
+  },
+  [GET_NEW_DATA] (state, action) {
+    let response = action.payload
+    let result = Object.keys(response).map(dateKey => {
+      return {
+        date: dateKey,
+        num: response[dateKey]
+      }
+    })
+    return {
+      ...state,
+      plantLogs: result
+    }
+  },
+  [REQUEST_BEGIN] (state) {
+    console.log('begin__reducer')
+    return {
+      ...state,
+      requesting: true
+    }
+  },
+  [REQUEST_FINISH] (state) {
+    return {
+      ...state,
+      requesting: false
+    }
   }
 }, {
+  requesting: false,
   plantLogs: []
 })
